65af77cf57368d4ea73cb20cf96d8553494ace10
MatiFreyre <mfreyre@gmail.com>
2024-06-21 00:37:57 -0300

    first commit

---
+++ teg.pl
@@ -0,0 +1,77 @@
+/* distintos paises */
+paisContinente(americaDelSur, argentina).
+paisContinente(americaDelSur, bolivia).
+paisContinente(americaDelSur, brasil).
+paisContinente(americaDelSur, chile).
+paisContinente(americaDelSur, ecuador).
+paisContinente(europa, alemania).
+paisContinente(europa, espania).
+paisContinente(europa, francia).
+paisContinente(europa, inglaterra).
+paisContinente(asia, aral).
+paisContinente(asia, china).
+paisContinente(asia, gobi).
+paisContinente(asia, india).
+paisContinente(asia, iran).
+
+/*países importantes*/
+paisImportante(argentina).
+paisImportante(kamchatka).
+paisImportante(alemania).
+
+/*países limítrofes*/
+limitrofes([argentina,brasil]).
+limitrofes([bolivia,brasil]).
+limitrofes([bolivia,argentina]).
+limitrofes([argentina,chile]).
+limitrofes([espania,francia]).
+limitrofes([alemania,francia]).
+limitrofes([nepal,india]).
+limitrofes([china,india]).
+limitrofes([nepal,china]).
+limitrofes([afganistan,china]).
+limitrofes([iran,afganistan]).
+
+/*distribución en el tablero */
+ocupa(argentina, azul, 4).
+ocupa(bolivia, rojo, 1).
+ocupa(brasil, verde, 4).
+ocupa(chile, negro, 3).
+ocupa(ecuador, rojo, 2).
+ocupa(alemania, azul, 3).
+ocupa(espania, azul, 1).
+ocupa(francia, azul, 1).
+ocupa(inglaterra, azul, 2). 
+ocupa(aral, negro, 2).
+ocupa(china, verde, 1).
+ocupa(gobi, verde, 2).
+ocupa(india, rojo, 3).
+ocupa(iran, verde, 1).
+
+/*continentes*/
+continente(americaDelSur).
+continente(europa).
+continente(asia).
+
+/*objetivos*/
+objetivo(rojo, ocuparContinente(asia)).
+objetivo(azul, ocuparPaises([argentina, bolivia, francia, inglaterra, china])).
+objetivo(verde, destruirJugador(rojo)).
+objetivo(negro, ocuparContinente(europa)).
+
+% 1
+estaEnContinente(Jugador, Continente):-
+    ocupa(Pais, Jugador, _ ),
+    paisContinente(Continente, Pais).
+
+% 2
+ocupaContinente(Jugador, Continente):-
+    continente(Continente),
+    jugador(Jugador),
+    not((estaEnContinente(OtroJugador, Continente), Jugador \= OtroJugador)).
+ocupaContinente2(Jugador, Continente):-
+    continente(Continente),
+    jugador(Jugador),
+    forall(paisContinente(Continente, Pais), ocupa(Pais, Jugador, _)).
+
+jugador(Jugador):- objetivo(Jugador, _).
