953eb87c30f7a6b49055a61020be2839f40dadff
MatiFreyre <mfreyre@gmail.com>
2024-06-22 09:38:05 -0300

    Agregado de enunciado al código como comentario

--- teg.pl
+++ teg.pl
@@ -60,11 +60,16 @@ objetivo(verde, destruirJugador(rojo)).
 objetivo(negro, ocuparContinente(europa)).
 
 % 1
+% estaEnContinente/2: Relaciona un jugador y un continente si el jugador ocupa al menos un país en el continente
 estaEnContinente(Jugador, Continente):-
     ocupa(Pais, Jugador, _ ),
     paisContinente(Continente, Pais).
 
+% 2
+% cantidadPaises/2: Relaciona a un jugador con la cantidad de países que ocupa.
+
 % 3
+% ocupaContinente/2: Relaciona un jugador y un continente si el jugador ocupa totalmente al continente.
 ocupaContinente(Jugador, Continente):-
     continente(Continente),
     jugador(Jugador),
@@ -75,3 +80,30 @@ ocupaContinente2(Jugador, Continente):-
     forall(paisContinente(Continente, Pais), ocupa(Pais, Jugador, _)).
 
 jugador(Jugador):- objetivo(Jugador, _).
+
+% 4 
+% leFaltaMucho/2: Relaciona a un jugador y un continente si al jugador le falta 
+% ocupar más de 2 países de dicho continente.
+
+% 5 
+% sonLimitrofes/2: Relaciona 2 países si son limítrofes.
+
+% 6 
+/* esGroso/1: Un jugador es groso si cumple algunas de estas condiciones:
+- ocupa todos los países importantes,
+- ocupa más de 10 países
+- o tiene más de 50 ejercitos.
+*/
+
+% 7
+% estaEnElHorno/1: un país está en el horno si todos sus países limítrofes están ocupados
+% por el mismo jugador que no es el mismo que ocupa ese país.
+
+% 8
+% esCaotico/1: un continente es caótico si hay más de tres jugadores en el.
+
+% 9
+% capoCannoniere/1: es el jugador que tiene ocupado más países.
+
+% 10
+% ganadooor/1: un jugador es ganador si logro su objetivo 
